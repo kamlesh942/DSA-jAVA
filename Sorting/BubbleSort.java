4ef57e353b085119231f094143a26a24f159243f
kamlesh rajak <145656253+kamlesh942@users.noreply.github.com>
2025-12-31 17:31:04 +0000

    Add BubbleSort implementation

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+public class BubbleSort {
+
+    public static void sort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {5, 4, 1, 3, 6, 2};
+
+        sort(arr);
+        printArray(arr);
+    }
+}
